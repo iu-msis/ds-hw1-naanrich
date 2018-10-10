5c82b6b1dfd0bd2482485fb2947c4ef0c8f84c84
NATALIE RICHARDSON <natalierichardson@NATALIEs-MacBook-Air.local>
2018-10-09 20:15:27 -0400

    fixing comment

--- public/api/comment.php
+++ public/api/comment.php
@@ -1,7 +1,7 @@
 <?php
 require '../../app/common.php';
 // 1. Go to the database and get all teams
-$teams = Comment::fetchAll();
+$comment = Comment::fetchAll();
 // 2. Convert to JSON
 $json = json_encode($comment, JSON_PRETTY_PRINT);
 // 3. Print
